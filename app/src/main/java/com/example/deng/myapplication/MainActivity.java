af6e7a4839a3fc468298e1a462562ff0c94166f1
dengrongkang <deng_9@outlook.com>
2019-01-01 22:51:48 +0800

    end 1.1

--- app/src/main/java/com/example/deng/myapplication/MainActivity.java
+++ app/src/main/java/com/example/deng/myapplication/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.deng.myapplication;
 
 import android.media.Image;
 import android.media.MediaPlayer;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         intUi();
+        Button myButton=(Button) findViewById(R.id.btn);
+        chenge(myButton);
+    }
+    public void chenge(Button myButton){
+        myButton.setText("Play/Stop");
     }
     public void intUi(){
         final MediaPlayer mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.shui);
